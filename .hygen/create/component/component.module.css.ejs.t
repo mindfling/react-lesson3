dc15f4d4c3486fd74e4297e625241f91cf8b775c
Nepochtov Dmitry <dimendroider@gmail.com>
2025-10-29 02:51:32 +0300

    hygen test

--- .hygen/create/component/component.module.css.ejs.t
+++ .hygen/create/component/component.module.css.ejs.t
@@ -1,6 +1,20 @@
 ---
 to: <%= absPath %>/<%= component_name %>.module.css
 ---
+
+
+.<%= component_name %> {
+  
+}
+
+.<%= lower_name %> {
+  
+}
+
+.<%= lower_name %>_container {
+  
+}
+
 .container {
   
 }
--- .hygen/create/component/index.js
+++ .hygen/create/component/index.js
@@ -1,24 +1,53 @@
 module.exports = {
-  prompt: ({inquirer}) => {
+  prompt: ({ inquirer }) => {
     const questions = [
       {
-        type: 'input',
-        name: 'component_name',
-        message: 'Имя компонента',
+        type: "input",
+        name: "component_name",
+        message: "Имя компонента",
       },
       {
-        type: 'input',
-        name: 'dir',
-        message: 'Вложенная директория?(Optional)'
-      }
-    ]
+        type: "input",
+        name: "dir",
+        message: "Nested directory? (Optional) (by default)",
+      },
+      {
+        type: "input",
+        name: "test",
+        message: "THis is test input first (by default)",
+      },
+      {
+        type: "input",
+        name: "inputtest",
+        message: "THis is test input two (by default)",
+      },
+      {
+        type: "select",
+        name: "category",
+        message: "What is test category",
+        choices: [
+          "makes",
+          "done",
+          "atoms",
+          "molecules",
+          "organisms",
+          "templates",
+          "pages",
+        ],
+      },
+    ];
 
-
-    return inquirer.prompt(questions).then(answers => {
-      const {component_name, dir} = answers;
-      const path = `${dir ? `${dir}/` : ''}${component_name}`;
+    return inquirer.prompt(questions).then((answers) => {
+      const { component_name, dir } = answers;
+      const { test, inputtest, category } = answers;
+      console.log("answers: ", answers);
+      const lower_name = component_name.toString().toLowerCase();
+      console.log("lower_name: ", lower_name);
+      const path = `${dir ? `${dir}/` : ""}${component_name}`;
+      console.log("path: ", path);
       const absPath = `src/components/${path}`;
-      return {...answers, path, absPath};
+      console.log("absPath: ", absPath);
+      return { ...answers, path, absPath, lower_name, test, inputtest, category };
     });
-  }
-}
+  },
+};
