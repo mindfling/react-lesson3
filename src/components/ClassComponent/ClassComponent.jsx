dacd4d9fad97a60ca054ee8ed3d1542691e9f004
Nepochtov Dmitry <dimendroider@gmail.com>
2025-10-31 08:48:24 +0300

    arrow onSubmit

--- src/components/ClassComponent/ClassComponent.jsx
+++ src/components/ClassComponent/ClassComponent.jsx
@@ -1,17 +1,31 @@
-import React from 'react';
+import React, {Component} from 'react';
 import style from './ClassComponent.module.css';
 
-export class ClassComponent extends React.Component {
+export class ClassComponent extends Component {
+  constructor(props) {
+    super(props);
+    console.log(props);
+    this.state = {
+      number: 55,
+    };
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    console.log('submit', this.state.number);
+  }
+
+
   render() {
     return (
       <div className={style.game}>
-        <p className={style.result}>5</p>
-        <form className={style.form}>
+        <p className={style.result}>{this.state.number}</p>
+        <form className={style.form} onSubmit={(e) => this.handleSubmit(e)}>
           <label className={style.label} htmlFor="user_number">
             Угадай число
           </label>
           <input className={style.input} type="number" id="user_number" />
-          <button className={style.btn}>Угадать</button>
+          <button className={style.btn} type='submit'>Угадать</button>
         </form>
       </div>
     );
