42a540149243f53fed57e7630b6b6d766f9aeff8
Nepochtov Dmitry <dimendroider@gmail.com>
2025-10-31 08:26:08 +0300

    update ClassComponent from source

--- src/components/ClassComponent/ClassComponent.jsx
+++ src/components/ClassComponent/ClassComponent.jsx
@@ -1,46 +1,19 @@
 import React from 'react';
 import style from './ClassComponent.module.css';
 
-// * Классовый компонент
 export class ClassComponent extends React.Component {
-  constructor(props) {
-    super();
-    this.state = {
-      result: 4,
-      number: 5,
-    };
-    console.log('constructor ClassComponent');
-  }
-
-  handleSubmit(e) {
-    e.preventDefault();
-    console.log(e);
-    // this.state = 32;
-    this.setState(32);
-  }
-
-  handleClick(e) {
-    console.log('click', e);
-    console.log('state', this.state.number);
-  }
-
   render() {
     return (
       <div className={style.game}>
-        <p className={style.result}>{this.state.result}</p>
-        <form className={style.form} onSubmit={(e) => this.handleSubmit(e)}>
+        <p className={style.result}>5</p>
+        <form className={style.form}>
           <label className={style.label} htmlFor="user_number">
             Угадай число
           </label>
           <input className={style.input} type="number" id="user_number" />
-          <button className={style.btn}
-            onClick={this.handleClick}
-            title="Press button to guise">
-            Угадать
-          </button>
+          <button className={style.btn}>Угадать</button>
         </form>
       </div>
     );
   }
 }
-
