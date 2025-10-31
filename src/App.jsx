9539d487733e9b59794b60effc55e2f3fde72a37
Nepochtov Dmitry <dimendroider@gmail.com>
2025-10-31 19:10:55 +0300

    random number state handle submit change reset

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,12 @@ import React from 'react';
 import ComponentClass from './components/ClassComponent';
 
 export default class App extends React.Component {
-  
   render() {
     console.log('Hallo App class component');
-    
+
     return (
-      <div className='app'>
-        <ComponentClass />
+      <div className="app">
+        <ComponentClass my="this is my" text="hallllo" />
       </div>
     );
   }
--- src/components/ClassComponent/ClassComponent.jsx
+++ src/components/ClassComponent/ClassComponent.jsx
@@ -4,28 +4,69 @@ import style from './ClassComponent.module.css';
 export class ClassComponent extends Component {
   constructor(props) {
     super(props);
-    console.log(props);
     this.state = {
-      number: 55,
+      min: 1,
+      max: 10,
+      number: Math.floor(Math.random() * this.state.max - this.state.min) +
+        this.state.max,
+      userNumber: ' ',
     };
   }
 
   handleSubmit(e) {
-    e.preventDefault();
-    console.log('submit', this.state.number);
+    console.log('form submit target: ', e.target);
+    this.setState({
+      number: 12345,
+      result: 'Щас результат',
+    });
   }
 
+  handleChange(e) {
+    console.log('input change', e);
+    console.log('input target', e.target);
+    console.log('input value', e.target.value);
+    this.setState({
+      userNumber: e.target.value,
+    });
+    console.log(this.state);
+  }
+
+  handleReset(e) {
+    console.log('form reset', e);
+  }
 
   render() {
     return (
       <div className={style.game}>
-        <p className={style.result}>{this.state.number}</p>
-        <form className={style.form} onSubmit={(e) => this.handleSubmit(e)}>
+        <p className={style.result}>{`результат ${this.state.userNumber}`}</p>
+        <form
+          className={style.form}
+          onSubmit={(e) => {
+            this.handleSubmit(e);
+            e.preventDefault();
+          }}
+        >
           <label className={style.label} htmlFor="user_number">
             Угадай число
           </label>
-          <input className={style.input} type="number" id="user_number" />
-          <button className={style.btn} type='submit'>Угадать</button>
+
+          <input
+            className={style.input}
+            type="number"
+            id="user_number"
+            name="userNumber"
+            onChange={(e) => {
+              this.handleChange(e);
+            }}
+          />
+
+          <button className={style.btn} type="submit">
+            Угадать
+          </button>
+
+          <button className={style.btn} type="reset">
+            Очистить
+          </button>
         </form>
       </div>
     );
